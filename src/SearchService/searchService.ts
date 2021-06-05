016c41622afeefc41bb2a5c4fa8709d3dad84fe7
Dennis Ek <dennis.a.ek@gmail.com>
2021-06-05 22:05:46 +0200

    Implement SearchService.SearchAdvanced

---
+++ src/SearchService/searchService.ts
@@ -0,0 +1,72 @@
+import { Service } from "../service";
+
+export class SearchService extends Service {
+    public async SearchAdvanced(request: SearchAdvancedRequest) {
+        return this.callApiMethod<SearchResult>("SearchAdvanced", "SearchAdvancedResult", request);
+    }
+}
+
+type Error = {
+    Code: string;
+    Message: string;
+};
+
+type SearchAdvancedRequest = {
+    request: {
+        SearchWords?: string;
+        CategoryId?: number;
+        SearchInDescription?: boolean;
+        Mode?: "Or" | "Exact" | "And";
+        PriceMinimum?: number | null;
+        PriceMaximum?: number | null;
+        BidsMinimum?: number | null;
+        BidsMaximum?: number | null;
+        ZipCode?: string;
+        CountyId?: number;
+        Alias?: string | null;
+        OrderBy?:
+            | "Relevance"
+            | "BidsAscending"
+            | "BidsDescending"
+            | "PriceAscending"
+            | "PriceDescending"
+            | "EndDateAscending"
+            | "EndDateDescending"
+            | "StartDateDescending"
+            | "DsrAverage";
+        ItemStatus?: "Ended" | "Active";
+        ItemType?: "All" | "Auction" | "BuyItNow";
+        OnlyAuctionsWithBuyNow?: boolean;
+        OnlyItemsWithThumbnail?: boolean;
+        ItemsPerPage?: number;
+        PageNumber?: number;
+        ItemCondition?: "All" | "OnlySecondHand" | "OnlyNew";
+        SellerType?: "All" | "Private" | "Company";
+        Brands?: string[];
+        CampaignCodeIds?: number[];
+    };
+};
+
+type SearchItem = {
+    Id: number;
+    ShortDescription: string;
+    CategoryId: number;
+    BuyItNowPrice: number;
+    SellerId: number;
+    SellerAlias: string;
+    MaxBid: number;
+    ThumbnailLink: string;
+    SellerDsrAverage: number;
+    EndDate: Date;
+    NextBid: number;
+    HasBids: boolean;
+    IsEnded: boolean;
+    ItemType: "Auction" | "AuctionWithBuyItNow" | "PureBuyItNow" | "ShopItem";
+};
+
+type SearchResult = {
+    TotalNumberOfItems: number;
+    TotalNumberOfPages: number;
+    Items: SearchItem[];
+    Errors: Error[];
+};
--- src/traderaApiClient.ts
+++ src/traderaApiClient.ts
@@ -1,35 +1,45 @@
-import { Client, createClientAsync } from "soap";
+import { createClientAsync } from "soap";
 import { PublicService } from "./PublicService/publicService";
+import { SearchService } from "./SearchService/searchService";
 
 export class TraderaApiClient {
-    private readonly publicServiceUrl = "https://api.tradera.com/v3/PublicService.asmx";
-    private soapClient: Client | undefined;
+    private readonly publicServiceUrl = "https://api.tradera.com/v3/publicservice.asmx";
+    private readonly searchServiceUrl = "https://api.tradera.com/v3/searchservice.asmx";
     // public readonly buyerService: BuyerService;
     // public readonly listingService: ListingService;
     // public readonly orderService: OrderService;
     public readonly publicService: PublicService;
     // public readonly restrictedService: RestrictedService;
-    // public readonly searchService: SearchService;
+    public readonly searchService: SearchService;
 
     public constructor(private readonly appId: number, private readonly appKey: string) {
         this.publicService = new PublicService();
+        this.searchService = new SearchService();
     }
 
     public async init() {
-        this.soapClient = await createClientAsync(
-            this.wdsl(),
-            undefined,
-            this.publicServiceUrlWithAuth(),
+        this.publicService.setSoapClient(
+            await createClientAsync(
+                this.wdsl(this.publicServiceUrl),
+                undefined,
+                this.serviceUrlWithAuth(this.publicServiceUrl),
+            ),
         );
-        this.publicService.setSoapClient(this.soapClient);
+        this.searchService.setSoapClient(
+            await createClientAsync(
+                this.wdsl(this.searchServiceUrl),
+                undefined,
+                this.serviceUrlWithAuth(this.searchServiceUrl),
+            ),
+        );
     }
 
-    private wdsl() {
-        return `${this.publicServiceUrl}?WSDL`;
+    private wdsl(serviceUrl: string) {
+        return `${serviceUrl}?WSDL`;
     }
 
-    private publicServiceUrlWithAuth() {
-        const url = new URL(this.publicServiceUrl);
+    private serviceUrlWithAuth(serviceUrl: string) {
+        const url = new URL(serviceUrl);
         url.searchParams.append("appId", this.appId.toString());
         url.searchParams.append("appKey", this.appKey);
         return url.toString();
